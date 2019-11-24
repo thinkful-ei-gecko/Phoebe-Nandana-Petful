b3d62c9435d61a10377261ac860e8c67d9d372b4
Phoebe Law <phoebebasilio@gmail.com>
2019-11-24 14:19:11 -0800

    Updates config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
 	API_ENDPOINT:
-		process.env.NODE_ENV === "development"
+		process.env.NODE_ENV === "production"
 			? process.env.REACT_APP_ENDPOINT
 			: 'http://localhost:8080/api' //WHEN YOU EDIT THIS MAKE SURE YOU UPDATE THE URL LOCATIONS FOR THE CALLS //pbtag
 	// API_TOKEN: process.env.REACT_APP_API_TOKEN
