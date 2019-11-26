6f1dac68fb40bfc35bdd57cb0706f0d73bb0e74d
Phoebe Law <phoebebasilio@gmail.com>
2019-11-25 16:32:02 -0800

    updates config file from testing deployed server

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export default {
 	API_ENDPOINT:
 		process.env.NODE_ENV === "production"
 			? process.env.REACT_APP_ENDPOINT
-			: "https://ancient-everglades-87501.herokuapp.com/api" //WHEN YOU EDIT THIS MAKE SURE YOU UPDATE THE URL LOCATIONS FOR THE CALLS //pbtag
+			: "https://localhost:8080//api" //WHEN YOU EDIT THIS MAKE SURE YOU UPDATE THE URL LOCATIONS FOR THE CALLS //pbtag
 	// API_TOKEN: process.env.REACT_APP_API_TOKEN
 };
