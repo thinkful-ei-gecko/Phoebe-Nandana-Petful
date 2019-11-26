62f1954b2f3516395a09ed8ae8e25b5cabce96ca
Phoebe Law <phoebebasilio@gmail.com>
2019-11-25 19:33:14 -0800

    Uses right endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
 	API_ENDPOINT:
 		process.env.NODE_ENV === "production"
-			? process.env.REACT_APP_ENDPOINT
+			? process.env.REACT_APP_API_ENDPOINT
 			: "https://localhost:8080//api" //WHEN YOU EDIT THIS MAKE SURE YOU UPDATE THE URL LOCATIONS FOR THE CALLS //pbtag
 	// API_TOKEN: process.env.REACT_APP_API_TOKEN
 };
